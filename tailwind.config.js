b4ab9355d9c312d530b22873be2c8d1d90f7922b
Equipo2\c.valenzuela <carlosvalenzuelag93@gmail.com>
2025-05-09 13:29:41 +0200

    feat: responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./app/**/*.html', './app/**/*.js', './app/**/*.jsx', './app/**/*.tsx'],
   theme: {
-      extend: {},
+    extend: {
+      screens: {
+        'below400': { max: '399px' },
+      },
+    },
   },
   plugins: [],
 };
